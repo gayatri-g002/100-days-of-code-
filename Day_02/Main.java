cc983719a9ca19c681ee7f70bc3e7f13a3bcfa6f
Gayatri Mistary <gayatri.mistary2021@vitbhopal.ac.in>
2023-11-27 22:22:52 +0530

    Add files via upload

---
+++ Day_02/Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+   public static void main(String[] args) {
+      try (Scanner input = new Scanner(System.in)) {
+         System.out.println(input.nextLine());
+      }
+   }
+}
---
+++ Day_02/primitives.java
@@ -0,0 +1,12 @@
+public class primitives {
+    public static void main(String[] args) {
+         
+        int rollno = 64;
+        String name = "Gayatri";
+
+        System.out.println("Roll number:"+ rollno);
+        System.out.println("Name:"+ name);
+        
+    }
+    
+}
